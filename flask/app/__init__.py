645bb358f96b39743c9fd0d59ad7f22440b06e78
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-10-17 16:39:18 -0700

    Added new edits on app 2

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# api = Api(app)
+
 from app import views
--- flask/run.py
+++ flask/run.py
@@ -7,5 +7,13 @@
 
 from app import app
 
+from flask_restful import Resource, Api
+from app.views import EnvName, Item, AddItem
+api = Api(app)
+api.add_resource(EnvName, '/')
+api.add_resource(Item, '/items/<string:name>')
+api.add_resource(AddItem, '/items/Add')
+
+
 if __name__ == "__main__":
     app.run()
